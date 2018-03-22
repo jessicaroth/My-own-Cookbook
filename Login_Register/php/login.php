dc064b277478279ad434323c57ca30e52d73b4a4
RebGe <s160664@student.dhbw-mannheim.de>
2018-03-22 11:53:32 +0100

    Login

--- Login_Register/php/login.php
+++ Login_Register/php/login.php
@@ -1,11 +1,16 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=cookbookdb', 'root', '');
 
-//entered_username = feld.innerHTML oder so ähnlich
+ if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+      $pdo = new PDO('mysql:host=localhost;dbname=my_own_cookbook', 'root', '');
 
-$sql = "SELECT password FROM user WHERE username = entered_username";	//hier schauen, dass auch der Inhalt der Variable genommen wird
+      $email = "keine-ahnung@freenet.de";
 
-foreach ($pdo->query($sql) as $row) {
-   echo $row['username']." ".$row['password']."<br />";
-}
+      $statement = $pdo->prepare("SELECT password FROM user WHERE email = :email");
+      $statement->execute(array('email' => $email));   
+      while($row = $statement->fetch()) {
+        echo "Passwort: ".$row['password']."<br /><br />";
+      }
+    }
+
 ?>
--- Login_Register/php/register.php
+++ Login_Register/php/register.php
@@ -1,11 +1,11 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=cookbookdb', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=my_own_cookbook', 'root', '');
 
 $neuer_user = array();
 
 //hier dann eben entsprechend den Inhalt aus den Feldern ziehen
 
-$neuer_user['first_name'] = 'MArcel';	
+$neuer_user['first_name'] = 'Marcel';	
 $neuer_user['last_name'] = 'Vidas';
 $neuer_user['email'] = 'info@php-einfach.de';
 $neuer_user['password'] = 'fdnjkfjnaslk';
